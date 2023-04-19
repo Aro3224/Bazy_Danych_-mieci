768ed927b743921dd42c93de6f5cd2665c39c2ec
Arek Zwolenik <arek.zwolenik@o2.pl>
2023-04-19 11:15:33 +0200

    dodanie passwordu do administrators

--- Ecocoon/Ecocoon/Administrator.cs
+++ Ecocoon/Ecocoon/Administrator.cs
@@ -18,5 +18,6 @@ namespace Ecocoon
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
